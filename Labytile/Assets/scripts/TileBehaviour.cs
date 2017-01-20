2985c0d9eab6b0bb7e62749017c8fc2b4a38289a
TECH-INFO\edwin.saggia <galaxteam@hotmail.com>
2017-01-20 21:07:59 +0100

    mise en place des différents scripts + prefabs de tuiles et player

---
+++ Labytile/Assets/scripts/TileBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBehaviour : MonoBehaviour 
+{
+
+    public Transform player = null;
+    public float elevation = 30.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 playerPos = player.position;
+	
+	}
+}
