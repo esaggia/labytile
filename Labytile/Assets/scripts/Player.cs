d423026f4d8cc0875464ba44b90a5edf12b95fe8
Z4B31\auto <estherroux@yahoo.fr>
2017-01-20 21:39:36 +0100

    PlayerMove Camera Empty

--- Labytile/Assets/scripts/Player.cs
+++ Labytile/Assets/scripts/Player.cs
@@ -2,14 +2,37 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
+    public float velocityX;
+    public float velocityZ;
+    
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+    {
+
+       
+
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Vector3 movement = new Vector3(velocityX, 0.0f, 0.0f);
+            transform.position += movement;
+
+
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            Vector3 movement = new Vector3(0.0f,0.0f , velocityZ);
+            transform.position += movement;
+
+        }
+
+      
 	
 	}
 }
