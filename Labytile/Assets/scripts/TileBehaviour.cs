e6dfe51dfa02df4bdbda919ab34ac91573392a8b
TECH-INFO\edwin.saggia <galaxteam@hotmail.com>
2017-01-20 21:35:27 +0100

    tuiles s'enfoncent dans le sol si le player est tout pret (-> ok)

--- Labytile/Assets/scripts/TileBehaviour.cs
+++ Labytile/Assets/scripts/TileBehaviour.cs
@@ -3,21 +3,35 @@ using System.Collections;
 
 public class TileBehaviour : MonoBehaviour 
 {
-
+    public float playerViewRange = 20.0f;
     public Transform player = null;
     public float elevation = 30.0f;
+
+    private bool isElevated = false;
+    private float initialHeight = 0.0f;
 
 	// Use this for initialization
 	void Start () 
     {
+        player = FindObjectOfType<Player>().transform;
+        initialHeight = transform.position.y;
         
-
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         Vector3 playerPos = player.position;
-	
+
+        if (!isElevated && Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(playerPos.x, playerPos.z)) > playerViewRange)
+        {
+            isElevated = true;
+            transform.position += new Vector3(0.0f, elevation, 0.0f);
+        }
+        else if (isElevated && Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(playerPos.x, playerPos.z)) < playerViewRange)
+        {
+            isElevated = false;
+            transform.position = new Vector3(transform.position.x, initialHeight, transform.position.z);
+        }
 	}
 }
