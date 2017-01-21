74c470bacbf2ab171e139b0691b55460e8b178a3
Esther Roux <esther.roux13@hotmail.com>
2017-01-21 12:14:21 +0100

    map generator portail endDoor

---
+++ Labytile/Assets/scripts/Portail.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portail : MonoBehaviour {
+	public GameObject endPortal;
+	public Vector3 portalPos;
+
+	// Use this for initialization
+	void Start () {
+
+		 
+		 GameObject endDoor= (GameObject)Instantiate (endPortal,portalPos,transform.rotation);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
