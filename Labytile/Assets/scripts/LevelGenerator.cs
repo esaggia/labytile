819fa8972cf445c85d73d38432e258fb84995435
TECH-INFO\edwin.saggia <galaxteam@hotmail.com>
2017-01-21 02:44:34 +0100

    map générée ... a moitié, player non trouvé dans les data de l'image

--- Labytile/Assets/scripts/LevelGenerator.cs
+++ Labytile/Assets/scripts/LevelGenerator.cs
@@ -3,13 +3,96 @@ using System.Collections;
 
 public class LevelGenerator : MonoBehaviour {
 
+    public Texture2D levelTexture = null;
+    public GameObject movingTile = null;
+    public GameObject staticTile = null;
+    public GameObject playerObject = null;
+
+    public Player GetPlayer() { return player; }
+
+    private Player player = null;
+
+    private basicTile[] tiles;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
+
+        Color[] pixels = levelTexture.GetPixels();
+
+        tiles = new basicTile[pixels.Length];
+        int tileCounter = 0;
+
+        for (int i = 0; i < pixels.Length;i++)
+        {
+            int indexX = i % levelTexture.width;
+            int indexZ = i / levelTexture.width;
+
+            if (pixels[i].r == 0) // tiles 
+            {
+                basicTile tile = SpawnTile(movingTile, indexX, indexZ);
+                tiles[tileCounter] = tile;
+                tileCounter++;
+            }
+            else
+            {
+                basicTile tile = SpawnTile(staticTile, indexX, indexZ);
+                tiles[tileCounter] = tile;
+                tileCounter++;
+            }
+
+            //if (pixels[i].g == 1) // spawn d'ennemi
+            //    SpawnTile(movingTile, indexX, indexZ);
+            //
+            if (pixels[i].b == 1) // spawn joueur
+                SpawnPlayer(indexX, indexZ);
+            //
+            //    if (pixels[i].a == 1) { }// 
+            //    SpawnTile(movingTile, indexX, indexZ);
+
+        }
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+
+    basicTile SpawnTile(GameObject tileToSpawn,int indexX, int indexZ)
+    {
+        GameObject tile = Instantiate(tileToSpawn, transform.position + new Vector3((float)indexX * tileToSpawn.transform.localScale.x, 0.0f, (float)indexZ * tileToSpawn.transform.localScale.z), Quaternion.identity) as GameObject;
+        basicTile tileComp = tile.GetComponent<basicTile>();
+        tileComp.indexX = indexX;
+        tileComp.indexZ = indexZ;
+
+        return tileComp;
+    }
+
+    Player SpawnPlayer(int indexX, int indexZ)
+    {
+        basicTile tile = GetTileWithIndex(indexX,indexZ);
+        Transform spawnPoint = tile.transform.GetChild(0);
+
+        GameObject player = Instantiate(playerObject, spawnPoint.position, spawnPoint.rotation) as  GameObject;
+
+        Player playerComp = player.GetComponent<Player>();
+
+        varManager.player = playerComp;
+
+        return playerComp;
+    }
+
+    basicTile GetTileWithIndex(int indexX, int indexZ)
+    {
+        basicTile tileToReturn = null;
+
+        foreach (basicTile tile in tiles)
+        {
+            tileToReturn = tile.indexX == indexX ? (tile.indexZ == indexZ ? tile : null) : null;
+        }
+
+        return tileToReturn;
+    }
 }
---
+++ Labytile/Assets/scripts/movingTile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingTile : basicTile 
+{
+    public float elevation = 30.0f;
+    public float elevationSpeed = 5.0f;
+
+    private bool isElevated = true;
+
+	// Use this for initialization
+	protected override void Start ()
+    {
+        base.Start();
+
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        float dist = GetPlayerDistance();
+
+        if (!isElevated && dist > playerViewRange)
+        {
+            isElevated = true;
+            transform.position = new Vector3(transform.position.x, initialHeight, transform.position.z);
+        }
+        else if (isElevated && dist < playerViewRange)
+        {
+            isElevated = false;
+            transform.position -= new Vector3(0.0f, elevation, 0.0f);
+        }
+
+    }
+
+    IEnumerator MoveTile(bool moveDownWards = false)
+    {
+        //float startposY = transform.position.y;
+
+        //if (dow)
+        yield return null;
+    }
+}
