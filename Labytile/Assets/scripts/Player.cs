5e406ef3eedbc4d48af27ddfcf25f9c46663d18d
Esther Roux <esther.roux13@hotmail.com>
2017-01-21 01:32:13 +0100

    GetKey

--- Labytile/Assets/scripts/Player.cs
+++ Labytile/Assets/scripts/Player.cs
@@ -14,23 +14,23 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-       if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) 
+     if( Input.GetKey( KeyCode.UpArrow ) )
         {
         Vector3 movementR = new Vector3(velocityX,0.0f ,0.0f);
         transform.position += movementR;
         } 
-        else if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+        else if (Input.GetKey( KeyCode.DownArrow ))
         {
         Vector3 movementL = new Vector3(velocityX,0.0f ,0.0f );
         transform.position -= movementL;
         }
  
-        if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+        if (Input.GetKey( KeyCode.LeftArrow ))
         {   
             Vector3 movementR = new Vector3(0.0f,0.0f , velocityZ);
             transform.position -= movementR;
         }
-         else if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0) 
+         else   if (Input.GetKey( KeyCode.RightArrow ))
          { 
             Vector3 movementL = new Vector3(0.0f,0.0f , velocityZ);
             transform.position += movementL;
