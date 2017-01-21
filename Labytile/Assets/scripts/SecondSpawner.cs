e0dc71e8d96279d7ba3b289687d70ac897f8dfb3
Esther Roux <esther.roux13@hotmail.com>
2017-01-21 20:27:26 +0100

    spawn portal + (nme ? )

---
+++ Labytile/Assets/scripts/SecondSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondSpawner : MonoBehaviour {
+	public GameObject endPortal;
+	public Vector3 portalPos;
+
+	public GameObject nme;
+	public Vector3 nmePos;
+
+	// Use this for initialization
+	void Start () {
+	 GameObject endDoor= (GameObject)Instantiate (endPortal,portalPos,transform.rotation);
+	  GameObject nmeClone= (GameObject)Instantiate (nme,nmePos,transform.rotation);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
